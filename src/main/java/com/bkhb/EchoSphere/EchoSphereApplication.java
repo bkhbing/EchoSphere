6154bea58c6ae4c353964d174708807b90752b90
bkhb <1787626070@qq.com>
2024-01-08 17:21:36 +0800

    init obj
    初始化项目

---
+++ src/main/java/com/bkhb/EchoSphere/EchoSphereApplication.java
@@ -0,0 +1,13 @@
+package com.bkhb.EchoSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bkhb/EchoSphere/EchoSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkhb.EchoSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
