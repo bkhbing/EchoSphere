dbffebfb84f0af5471a0a99c2607396ae77ce893
1787626070@qq.com <584520fyh>
2024-03-29 13:39:00 +0800

    添加数据库字段自动添加

---
+++ src/main/java/com/bkhb/EchoSphere/handler/MyMetaObjectHandler.java
@@ -0,0 +1,29 @@
+package com.bkhb.EchoSphere.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * mybatis-plus 自动填充
+ * @author bkhb
+ * @since 2024-03-26 00:57:57
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐使用)
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update fill ....");
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐)
+    }
+}
